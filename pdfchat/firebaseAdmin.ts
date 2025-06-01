47d6fa849b3163c92e5dd3b7621dff9b97345034
Vamsi Krishna <saivamsikrishna3515@gmail.com>
2025-06-01 15:51:43 +0530

    modified service account in firebase admin

--- pdfchat/firebaseAdmin.ts
+++ pdfchat/firebaseAdmin.ts
@@ -2,13 +2,31 @@ import { getApp, getApps, initializeApp, cert, App } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 import { getStorage } from "firebase-admin/storage";
 
-const serviceKey = require('@/service_key.json');
+let serviceAccount;
+
+if (process.env.VERCEL) {
+  serviceAccount = {
+    type: process.env.FIREBASE_TYPE,
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: process.env.FIREBASE_AUTH_URI,
+    token_uri: process.env.FIREBASE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+    universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
+  };
+} else {
+  serviceAccount = require('@/service_key.json');
+}
 
 let app: App;
 
 if(getApps().length === 0){
     app = initializeApp({
-        credential: cert(serviceKey),
+        credential: cert(serviceAccount),
     })
 }else{
     app = getApp();
